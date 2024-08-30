bba22c49fd74ede24f8e881e5585caea1d0f9072
AnupkumarHM <72343309+AnupkumarHM@users.noreply.github.com>
2024-08-30 21:54:51 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const upload = require('../middleware/upload'); // Import the Multer middleware
-
+const upload = require('../middleware/upload'); 
 router.post('/register', upload.single('image'), userController.registerUser);
 router.get('/user', userController.getAllUsers);
 router.post('/login', userController.loginUser);
-
+router.post('/users', async (req, res))
+router.get('/users', async (req, res))
+router.get('/users/:id', async (req, res))
+router.put('/users/:id', async (req, res))
+router.delete('/users/:id', async (req, res))
 module.exports = router;
